9d64d040af9461a1f73911f17effcbaec6d99372
Artem Golovanov <115426628+temaHead@users.noreply.github.com>
2024-04-28 20:50:26 +0300

    add config

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,26 @@
+import { Configuration } from "webpack";
+import { BuildOptions } from "./types/config";
+import { buildLoader } from "./buildLoaders";
+import { buildPlugins } from "./buildPlugins";
+import { buildResolvers } from "./buildResolvers";
+import { buildDevServer } from "./buildDevServer";
+
+export function buildWebpackConfig(options: BuildOptions): Configuration {
+  const { paths, mode, isDev } = options;
+  return {
+    mode: mode,
+    entry: paths.entry,
+    output: {
+      filename: "[name].[contenthash].js",
+      path: paths.build,
+      clean: true,
+    },
+    plugins: buildPlugins(options),
+    module: {
+      rules: buildLoader(options),
+    },
+    resolve: buildResolvers(),
+    devtool: isDev ? "inline-source-map" : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
+  };
+}
