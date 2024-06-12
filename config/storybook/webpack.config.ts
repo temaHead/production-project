a2589fa931e08f8be52e652f638362f7bcb5b655
Artem Golovanov <115426628+temaHead@users.noreply.github.com>
2024-06-13 00:07:04 +0300

    storybook

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -12,8 +12,12 @@ export default ({ config }: { config: any }) => {
         locales: '',
         buildLocales: ''
     };
-    config.resolve.modules.push(paths.src);
-    config.resolve.extensions.push('.ts', '.tsx');
+    config!.resolve!.modules!.push(paths.src);
+    config!.resolve!.extensions!.push('.ts', '.tsx');
+    config!.resolve!.alias = {
+        ...config!.resolve!.alias,
+        '@': paths.src,
+    };
 
     // eslint-disable-next-line no-param-reassign
     config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
