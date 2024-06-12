1b61568fd9ff1607908cd500cdc7de9a85b007ea
Artem Golovanov <115426628+temaHead@users.noreply.github.com>
2024-06-12 15:18:31 +0300

    drawer

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -3,8 +3,10 @@ import {
     MutableRefObject,
     ReactNode, useCallback, useEffect, useRef, useState,
 } from 'react';
+import { useTheme } from 'app/providers/ThemeProvider';
 import cls from './Modal.module.scss';
 import Portal from '../Portal/Portal';
+import { Overlay } from '../Overlay/Overlay';
 
 interface ModalProps {
   className?: string;
@@ -24,6 +26,7 @@ function Modal(props: ModalProps) {
     const [isClosing, setIsClosing] = useState(false);
     const [isMounted, setIsMounted] = useState(false);
     const timerRef = useRef() as MutableRefObject<ReturnType<typeof setTimeout>>;
+    const { theme } = useTheme();
 
     const mods:Mods = {
         [cls.opened]: isOpen,
@@ -39,10 +42,6 @@ function Modal(props: ModalProps) {
             }, ANIMATION_DELAY);
         }
     }, [onClose]);
-
-    const onContentClick = (e:React.MouseEvent) => {
-        e.stopPropagation();
-    };
 
     const onKeyDown = useCallback((e:KeyboardEvent) => {
         if (e.key === 'Escape') {
@@ -71,14 +70,12 @@ function Modal(props: ModalProps) {
     }
     return (
         <Portal>
-            <div className={classNames(cls.Modal, mods, [className])}>
-                <div className={cls.overlay} onClick={closeHandler}>
-                    <div
-                        className={cls.content}
-                        onClick={onContentClick}
-                    >
-                        {children}
-                    </div>
+            <div className={classNames(cls.Modal, mods, [className, theme, 'app_modal'])}>
+                <Overlay onClick={closeHandler} />
+                <div
+                    className={cls.content}
+                >
+                    {children}
                 </div>
             </div>
         </Portal>
