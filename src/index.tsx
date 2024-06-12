a3f1b346ec4cf81735e9eea3f22d5cf75c1c95f7
Artem Golovanov <115426628+temaHead@users.noreply.github.com>
2024-06-12 18:11:56 +0300

    vite

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { ErrorBoundary } from '@/app/providers/ErrorBoundary';
 import { StoreProvider } from '@/app/providers/StoreProvider';
 import App from './app/App';
 import './shared/config/i18n/i18n';
-import './app/styles/index.scss';
+import '@/app/styles/index.scss';
 
 const container = document.getElementById('root');
 
